c0937753af569e08a32e4f73eae8702cc47e9127
TheusZp <matheuszoppe@hotmail.com>
2022-08-01 20:45:55 -0300

    correção link api

--- src/contexts/ApiPokemonContext.jsx
+++ src/contexts/ApiPokemonContext.jsx
@@ -1,35 +1,33 @@
 import React from "react";
-import axios from 'axios'
+import axios from "axios";
 
+export const ApiPokemonContext = React.createContext();
 
-export const ApiPokemonContext = React.createContext()
-
-export const ApiPokemonStorage = ({children}) =>{
+export const ApiPokemonStorage = ({ children }) => {
   // const pathname = window.location.pathname.replace("/", "");
   const [dados, setDados] = React.useState(null);
   const [pokemon, setPokemon] = React.useState(1);
-  const baseURL = import.meta.env.VITE_API_URL
-  React.useEffect(() =>{
-     
-    axios.get(baseURL + pokemon).then((reponse)=>{setDados(reponse.data)})
-   
-    
+  const baseURL = import.meta.env.VITE_API_URL;
+  React.useEffect(() => {
+    axios.get(baseURL + pokemon).then((reponse) => {
+      setDados(reponse.data);
+    });
+
     // async function fetchPokemon(pokemon) {
     //   const response = await fetch(
     //     `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-    //   );      
+    //   );
     //   if (response.status === 200) {
     //     const data = await response.json();
     //     setDados(data);
     //   } else setDados(null);
     // }
     // fetchPokemon(pokemon);
-  }, [pokemon])
+  }, [pokemon]);
 
-
-  return(
-    <ApiPokemonContext.Provider value={{setPokemon, dados, pokemon}}>
+  return (
+    <ApiPokemonContext.Provider value={{ setPokemon, dados, pokemon }}>
       {children}
     </ApiPokemonContext.Provider>
-  )
-}
+  );
+};
